f35aac5797a98053ae1fedb7eceb0522508db5c2
Sergio Peris <dev.sertxu@gmail.com>
2024-09-30 12:32:33 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,7 +71,7 @@
         <div class="max-w-screen-xl mx-auto">
             <h2 class="text-3xl text-neutral-200 text-center font-medium uppercase font-heading">My latest posts</h2>
             <div class="border-b-2 border-ocean w-32 mx-auto mt-2 mb-10"></div>
-            <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+            <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 lg:px-0">
                 @foreach($posts as $post)
                     <x-post :post="$post" />
                 @endforeach
@@ -88,7 +88,7 @@
         <div class="max-w-screen-xl mx-auto">
             <h2 class="text-3xl text-neutral-200 text-center font-medium uppercase font-heading">My latest projects</h2>
             <div class="border-b-2 border-ocean w-32 mx-auto mt-2 mb-10"></div>
-            <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+            <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 lg:px-0">
                 @foreach($projects as $project)
                     <x-project :project="$project" />
                 @endforeach
