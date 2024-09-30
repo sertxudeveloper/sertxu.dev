fc9876ce76fc91c2741ffde9937980ffb6d87f66
Sergio Peris <dev.sertxu@gmail.com>
2024-09-30 12:40:02 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="bg-dark-200 flex md:space-x-24 items-center justify-center py-32 min-h-screen bg-doodles flex-col md:flex-row space-y-12">
-        <img class="rounded-2xl w-72 block opacity-0 animate-slide-fade-in ![animation-delay:100ms]" src="{{ asset('dp.webp') }}" alt="Sergio Peris">
+        <img class="rounded-2xl w-48 md:w-72 block opacity-0 animate-slide-fade-in ![animation-delay:100ms]" src="{{ asset('dp.webp') }}" alt="Sergio Peris">
 
         <div class="text-center space-y-6 px-4 md:px-12">
             <h1 class="text-4xl md:text-5xl font-bold text-neutral-200 opacity-0 animate-slide-fade-in ![animation-delay:100ms]">Hi, I'm Sertxu</h1>
