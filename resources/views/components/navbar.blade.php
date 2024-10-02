9c5e7d5252746c4dfcda4b81810867db9c4c0c15
Sergio Peris <dev.sertxu@gmail.com>
2024-10-02 09:44:28 +0200

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
-<nav x-data="{ open: false }" class="py-2.5 top-0 sticky bg-dark-300 shadow-sm xl:px-0 px-6 z-30">
-    <div class="flex justify-between max-w-screen-lg mx-auto items-center relative z-30">
+<nav x-data="{ open: false }" class="top-0 sticky">
+    <div class="flex justify-between max-w-screen-lg mx-auto items-center relative py-2.5 px-6 bg-dark-300 shadow-sm xl:px-0 z-30">
         <a href="{{ route('home') }}" class="outline-none" wire:navigate>
             @persist('logo')
                 <img src="{{ asset('favicon.svg') }}" alt="Sertxu Dev" class="h-10">
@@ -12,9 +12,9 @@
         </div>
 
         <!-- Mobile menu opener -->
-        <div class="flex inset-y-0 items-center right-0 md:hidden absolute pr-3">
+        <div class="flex inset-y-0 items-center right-0 md:hidden absolute">
             <button
-                class="border border-neutral-700 p-2 rounded-md text-gray-400"
+                class="border border-neutral-800 p-2 rounded-md text-gray-500"
                 aria-label="Main menu" aria-expanded="false" @click="open = !open">
 
                 <!-- Icon menu closed -->
@@ -46,7 +46,7 @@
         x-transition:leave-start="transform opacity-100 translate-y-0"
         x-transition:leave-end="transform opacity-0 -translate-y-1/2">
 
-        <div class="px-2 pt-2 pb-3">
+        <div class="px-2 pt-2 pb-3 text-sm text-neutral-300 flex flex-col">
             {{ $slot }}
         </div>
     </div>
