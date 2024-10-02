12089758dd17867600cf2f811f1039ef2ae7b4fe
Sergio Peris <dev.sertxu@gmail.com>
2024-10-02 09:37:51 +0200

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ open: false }" class="py-2.5 top-0 sticky bg-dark-300 shadow-sm xl:px-0 px-6 z-30">
-    <div class="flex justify-between max-w-screen-lg mx-auto items-center">
+    <div class="flex justify-between max-w-screen-lg mx-auto items-center relative z-30">
         <a href="{{ route('home') }}" class="outline-none">
             @persist('logo')
                 <img src="{{ asset('favicon.svg') }}" alt="Sertxu Dev" class="h-10">
@@ -14,7 +14,7 @@
         <!-- Mobile menu opener -->
         <div class="flex inset-y-0 items-center right-0 md:hidden absolute pr-3">
             <button
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 outline-none bg-transparent ml-3"
+                class="border border-neutral-700 p-2 rounded-md text-gray-400"
                 aria-label="Main menu" aria-expanded="false" @click="open = !open">
 
                 <!-- Icon menu closed -->
@@ -22,7 +22,7 @@
                     x-transition:enter="transition ease-out duration-500"
                     x-transition:enter-start="transform opacity-0"
                     x-transition:enter-end="transform opacity-100">
-                    <x-icons.bars />
+                    <x-icons.bars class="h-6" />
                 </div>
 
                 <!-- Icon menu open -->
@@ -30,24 +30,24 @@
                     x-transition:enter="transition ease-out duration-500"
                     x-transition:enter-start="transform opacity-0"
                     x-transition:enter-end="transform opacity-100">
-                    <x-icons.times />
+                    <x-icons.times class="h-6" />
                 </div>
             </button>
         </div>
-        
-        <!-- Mobile menu -->
-        <div x-show="open"
-            class="absolute bg-dark-300 md:hidden transform w-screen z-10"
-            x-transition:enter="transition ease-out duration-500"
-            x-transition:enter-start="transform opacity-0 -translate-y-1/2"
-            x-transition:enter-end="transform opacity-100 translate-y-0"
-            x-transition:leave="transition ease-in duration-200"
-            x-transition:leave-start="transform opacity-100 translate-y-0"
-            x-transition:leave-end="transform opacity-0 -translate-y-1/2">
+    </div>
 
-            <div class="px-2 pt-2 pb-3">
-                {{ $slot }}
-            </div>
+    <!-- Mobile menu -->
+    <div x-show="open"
+        class="absolute bg-dark-300 md:hidden transform w-full z-10 top-0 left-0 mt-[60px] border-t border-neutral-800"
+        x-transition:enter="transition ease-out duration-500"
+        x-transition:enter-start="transform opacity-0 -translate-y-1/2"
+        x-transition:enter-end="transform opacity-100 translate-y-0"
+        x-transition:leave="transition ease-in duration-200"
+        x-transition:leave-start="transform opacity-100 translate-y-0"
+        x-transition:leave-end="transform opacity-0 -translate-y-1/2">
+
+        <div class="px-2 pt-2 pb-3">
+            {{ $slot }}
         </div>
     </div>
 </nav>
