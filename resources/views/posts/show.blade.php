783c7509f58028c1af6b278d150c4409866dd290
Sergio Peris <dev.sertxu@gmail.com>
2024-10-02 08:41:38 +0200

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <div class="fixed bg-dark-200 bg-doodles h-full w-full -z-10"></div>
-    <div class="bg-dark-100 max-w-screen-lg mt mx-auto px-10 py-12 my-48">
+    <div class="bg-dark-100 max-w-screen-lg mt mx-auto px-10 py-12 lg:my-48">
         <h1 class="text-4xl font-medium text-neutral-200 leading-snug">{{ $post->title }}</h1>
-        <div class="flex items-center mb-4 mt-3 space-x-3">
+        <div class="flex flex-col md:flex-row md:items-center mb-4 mt-3 md:space-x-3">
             @if($post->is_published)
                 <span class="text-sm text-neutral-300">Published at {{ $post->published_at->format('d M Y') }}</span>
             @else
                 <span class="text-sm text-neutral-300">Unpublished</span>
             @endif
-            <span class="text-neutral-300">&middot;</span>
+            <span class="text-neutral-300 hidden md:block">&middot;</span>
             <ul class="space-x-1">
                 @foreach($post->tags as $tag)
                     <li class="inline-block">
