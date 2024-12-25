7907d6b82b1aae8fcd0d2ac5db5ccb240609732f
Sergio Peris <dev.sertxu@gmail.com>
2024-12-25 20:10:31 +0100

    Added Storage Backup scheduled command

---
+++ app/Console/Commands/StorageBackupCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use ZipArchive;
+
+class StorageBackupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'storage:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Backup the storage folder';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->info('Backing up storage folder...');
+
+        $filename = 'backup-'.date('Y_m_d_His').'.zip';
+
+        $zip = new ZipArchive();
+        if ($zip->open(storage_path('app/backups/'.$filename), ZipArchive::CREATE) === true) {
+            $files = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator(storage_path('app/public')),
+                RecursiveIteratorIterator::SELF_FIRST
+            );
+
+            foreach ($files as $file) {
+                $file = str_replace('\\', '/', $file);
+
+                if (in_array(substr($file, strrpos($file, '/') + 1), ['.', '..'])) {
+                    continue;
+                }
+
+                if (is_dir($file) === true) {
+                    $zip->addEmptyDir(str_replace(storage_path('app/public').'/', '', $file));
+                } elseif (is_file($file) === true) {
+                    $zip->addFromString(str_replace(storage_path('app/public').'/', '', $file), file_get_contents($file));
+                }
+            }
+
+            $zip->close();
+        }
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,5 @@ use App\Console\Commands;
 Schedule::command(Commands\PublishScheduledPostsCommand::class)->everyMinute();
 Schedule::command(Commands\SitemapGenerateCommand::class)->daily();
 Schedule::command(Commands\SqliteBackupCommand::class)->daily();
+Schedule::command(Commands\StorageBackupCommand::class)->daily();
 Schedule::command(Commands\SqliteCompactDatabaseCommand::class, ['--connection' => 'sqlite'])->monthly();
