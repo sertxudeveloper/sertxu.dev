2082c29bd7f9efc94648069ee4b901220af6c8c3
Sergio Peris <dev.sertxu@gmail.com>
2024-12-26 23:19:48 +0100

    Update about.blade.php

--- resources/views/home/partials/about.blade.php
+++ resources/views/home/partials/about.blade.php
@@ -5,8 +5,8 @@
         <div class="text-lg space-y-4 text-neutral-300 mx-auto max-w-screen-md px-4 lg:px-0">
             <p>My name is Sergio Peris, however I'm also known as Sertxu.
             <p>I'm a full&#8209;stack developer based in Spain, I love building web apps and making people's life easier.</p>
-            <p>I have experience working with a variety of technologies, including Laravel, Node.js and C#. I'm also an open&#8209;source maintainer and content creator, and I love sharing my knowledge with others.</p>
-            <p>I'm always looking for new challenges to try and learn new technologies to grow as a developer.</p>
+            <p>I have experience working with a variety of technologies, including Laravel, Node.js and C#. I'm also an open&#8209;source maintainer and content creator, I love sharing my knowledge with others.</p>
+            <p>I'm always looking for new challenges to try and learn new technologies to improve.</p>
         </div>
     </div>
 </section>
