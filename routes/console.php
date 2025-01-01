c1f6fbdde2b9d6095fd99bb1fe7503edb4b3ee79
Sergio Peris <dev.sertxu@gmail.com>
2025-01-01 16:10:39 +0100

    Solved Sentry issue "SERTXUDEV-D"

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Schedule::command(Commands\PublishScheduledPostsCommand::class)->everyMinute();
 Schedule::command(Commands\SitemapGenerateCommand::class)->daily();
 Schedule::command(Commands\SqliteBackupCommand::class)->daily();
 Schedule::command(Commands\StorageBackupCommand::class)->daily();
-Schedule::command(Commands\SqliteCompactDatabaseCommand::class, ['--connection' => 'sqlite'])->monthly();
+Schedule::command(Commands\SqliteCompactDatabaseCommand::class, ['connection' => 'sqlite'])->monthly();
 Schedule::command(Commands\ThreadsRefreshTokensCommand::class)->daily();
