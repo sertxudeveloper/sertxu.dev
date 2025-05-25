17bafa73bd96dab560bd4f53d03f8aae2599ac71
Sergio Peris <dev.sertxu@gmail.com>
2025-05-25 19:01:41 +0200

    Disabled SQLite maintenance commands

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,7 @@ use App\Console\Commands;
 
 Schedule::command(Commands\PublishScheduledPostsCommand::class)->everyMinute();
 Schedule::command(Commands\SitemapGenerateCommand::class)->daily();
-Schedule::command(Commands\SqliteBackupCommand::class)->daily();
-Schedule::command(Commands\StorageBackupCommand::class)->daily();
-Schedule::command(Commands\SqliteCompactDatabaseCommand::class, ['sqlite'])->monthly();
+// Schedule::command(Commands\SqliteBackupCommand::class)->daily();
+// Schedule::command(Commands\StorageBackupCommand::class)->daily();
+// Schedule::command(Commands\SqliteCompactDatabaseCommand::class, ['sqlite'])->monthly();
 Schedule::command(Commands\ThreadsRefreshTokensCommand::class)->daily();
