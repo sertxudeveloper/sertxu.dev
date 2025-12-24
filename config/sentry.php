a3180abd8d45c534349f0021c917c4fe24e02942
Sergio Peris <dev.sertxu@gmail.com>
2025-12-24 14:04:19 +0100

    Update Sentry release version format

--- config/sentry.php
+++ config/sentry.php
@@ -18,7 +18,7 @@ return [
 
     // The release version of your application
     // Example with dynamic git hash: trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD'))
-    'release' => mb_trim(exec('git --git-dir '.base_path('.git').' log --pretty="%h" -n1 HEAD')),
+    'release' => 'sertxy.dev@' . mb_trim(exec('git --git-dir '.base_path('.git').' log --pretty="%h" -n1 HEAD')),
 
     // When left empty or `null` the Laravel environment will be used (usually discovered from `APP_ENV` in your `.env`)
     'environment' => env('SENTRY_ENVIRONMENT'),
