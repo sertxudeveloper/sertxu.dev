84f526ae7afa717a02b0612d6e8171f40961a6fc
Sergio Peris <dev.sertxu@gmail.com>
2026-03-31 11:58:19 +0200

    wip

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,4 @@ use App\Console\Commands;
 
 Schedule::command(Commands\PublishScheduledPostsCommand::class)->hourly();
 Schedule::command(Commands\SitemapGenerateCommand::class)->daily();
-// Schedule::command(Commands\SqliteBackupCommand::class)->daily();
-// Schedule::command(Commands\StorageBackupCommand::class)->daily();
-// Schedule::command(Commands\SqliteCompactDatabaseCommand::class, ['sqlite'])->monthly();
 Schedule::command(Commands\ThreadsRefreshTokensCommand::class)->daily();
