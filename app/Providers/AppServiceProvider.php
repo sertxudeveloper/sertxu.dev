9ca51a0d9180252fef356c53f90dbb1025357f60
Sergio Peris <dev.sertxu@gmail.com>
2026-04-07 11:53:32 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
-use Throwable;
 
 final class AppServiceProvider extends ServiceProvider
 {
