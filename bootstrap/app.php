e4ac7869be6e9d54b1381deaf8e2dbda41d7062c
Sergio Peris <dev.sertxu@gmail.com>
2026-05-30 17:12:17 +0200

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Route;
 use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -14,20 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function (): void {
-            Route::middleware('static')
-                ->group(base_path('routes/static.php'));
-        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '0.0.0.0/0');
         $middleware->preventRequestForgery(except: [
             '/contact',
-        ]);
-
-        $middleware->group('static', [
-            App\Http\Middleware\SetCacheControlHeader::class,
-            Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
-
-// use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * Redirect routes
@@ -13,18 +12,18 @@ use Illuminate\Support\Facades\Route;
  */
 Route::redirect('/blog/setting-up-a-kubernetes-cluster-with-microk8s', '/blog/setting-up-kubernetes-with-microk8s', 301);
 
-// Route::get('/', Controllers\HomeController::class)->name('home');
-// Route::post('/contact', [Controllers\ContactController::class, 'store'])->name('contact.store');
+Route::get('/', Controllers\HomeController::class)->name('home');
+Route::post('/contact', [Controllers\ContactController::class, 'store'])->name('contact.store');
 
 // Route::get('/education', [Controllers\EducationController::class, 'index'])->name('education.index');
 // Route::get('/experience', [Controllers\ExperienceController::class, 'index'])->name('experience.index');
 
-// Route::get('/projects', [Controllers\ProjectController::class, 'index'])->name('projects.index');
-// Route::get('/projects/{project:slug}', [Controllers\ProjectController::class, 'show'])->name('projects.show');
+Route::get('/projects', [Controllers\ProjectController::class, 'index'])->name('projects.index');
+Route::get('/projects/{project:slug}', [Controllers\ProjectController::class, 'show'])->name('projects.show');
 
-// Route::get('/blog', [Controllers\PostController::class, 'index'])->name('posts.index');
-// Route::get('/blog/{post:slug}', [Controllers\PostController::class, 'show'])->name('posts.show');
-// Route::get('/blog/{post:slug}/thumbnail', Controllers\PostThumbnailController::class)->name('posts.thumbnail');
+Route::get('/blog', [Controllers\PostController::class, 'index'])->name('posts.index');
+Route::get('/blog/{post:slug}', [Controllers\PostController::class, 'show'])->name('posts.show');
+Route::get('/blog/{post:slug}/thumbnail', Controllers\PostThumbnailController::class)->name('posts.thumbnail');
 Route::get('/blog/{post:slug}/preview', Controllers\PostPreviewController::class)->name('posts.preview');
 
 // Route::view('/uses', 'uses')->name('uses');
@@ -34,6 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/threads/callback', [Controllers\ThreadsAuthController::class, 'store'])->name('threads.auth-callback');
 });
 
-// Route::get('/sitemap.xml', fn () => response(Storage::disk('r2')->get('sitemap.xml'), 200, ['Content-Type' => 'application/xml']));
+Route::get('/sitemap.xml', fn () => response(Storage::disk('r2')->get('sitemap.xml'), 200, ['Content-Type' => 'application/xml']));
 
 Route::redirect('/login', '/admin/login')->name('login');
