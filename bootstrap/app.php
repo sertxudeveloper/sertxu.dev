05c56417394036556f07ee64b36a558b2b5dab33
Shift <shift@laravelshift.com>
2026-05-30 11:51:35 +0000

    Register `static` middleware group

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,11 +14,20 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('static')
+                ->group(base_path('routes/static.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '0.0.0.0/0');
         $middleware->preventRequestForgery(except: [
             '/contact',
+        ]);
+
+        $middleware->group('static', [
+            \App\Http\Middleware\SetCacheControlHeader::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
