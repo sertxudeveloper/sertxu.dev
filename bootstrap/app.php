97eb2da79bc6ce3227d78494f276e4d103a38877
Sergio Peris <dev.sertxu@gmail.com>
2026-05-30 13:27:14 +0200

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '0.0.0.0/0');
+        $middleware->preventRequestForgery(except: [
+            '/contact',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         Integration::handles($exceptions);
