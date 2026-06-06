7dfb84a22e49c37fc4564bd68c7cb3c26d13130e
Sergio Peris <dev.sertxu@gmail.com>
2026-06-06 13:52:45 +0200

    wip

--- resources/views/home/partials/projects.blade.php
+++ resources/views/home/partials/projects.blade.php
@@ -43,5 +43,14 @@
                 </div>
             @endforeach
         </div>
+
+        <div class="mt-8 text-center" x-reveal>
+            <a href="{{ route('projects.index') }}" class="inline-flex items-center gap-2 px-6 py-3 border border-[#e02a3f] text-[#e02a3f] rounded-lg text-sm font-medium hover:bg-[#e02a3f] hover:text-white transition-all duration-200">
+                View all projects
+                <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M5 12h14M12 5l7 7-7 7" />
+                </svg>
+            </a>
+        </div>
     </div>
 </section>
